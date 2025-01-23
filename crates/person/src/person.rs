2a4cac1db274cda99ac76a2d92513d22998068c3
Oliver With <oliverwith@bbv.ch>
2025-01-23 11:53:16 +0100

    basic rust person library

---
+++ crates/person/src/person.rs
@@ -0,0 +1,66 @@
+extern crate chrono;
+use crate::chrono::Datelike;
+
+pub struct Person {
+    name: String,
+    zip: String,
+    dob: u32, //todo use chrono::NaiveDate
+    id: u32,  // in future may be hash of above
+}
+
+impl Person {
+    pub fn new(name: &str, zip: &str, dob: u32) -> Person {
+        Person {
+            name: name.to_string(),
+            zip: zip.to_string(),
+            dob,
+            id: 0,
+        }
+    }
+
+    pub fn get_age(&self) -> u32 {
+        let current_year = chrono::Utc::now().naive_utc().year_ce().1 as u32;
+        current_year - self.dob
+    }
+
+    pub fn get_zip(&self) -> &str {
+        &self.zip.as_str()
+    }
+
+    pub fn update_zip(&mut self, zip: &str) {
+        self.zip = zip.to_string();
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_person() {
+        let p = Person::new("John Doe", "12345", 1980);
+        assert_eq!(p.name, "John Doe");
+        assert_eq!(p.zip, "12345");
+        assert_eq!(p.id, 0);
+        assert_eq!(p.dob, 1980);
+    }
+
+    #[test]
+    fn get_person_age() {
+        let p = Person::new("John Doe", "12345", 1980);
+        assert_eq!(p.get_age(), 45);
+    }
+
+    #[test]
+    fn get_person_zip() {
+        let p = Person::new("John Doe", "12345", 1980);
+        assert_eq!(p.get_zip(), "12345");
+    }
+
+    #[test]
+    fn update_person_zip() {
+        let mut p = Person::new("John Doe", "12345", 1980);
+        p.update_zip("54321");
+        assert_eq!(p.get_zip(), "54321");
+    }
+}
